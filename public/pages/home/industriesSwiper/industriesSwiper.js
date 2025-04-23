975f084477f0adcd8dc14ae5c83398228a84042f
Kiltsov Anton <kiltsov.anton@gmail.com>
2025-04-23 13:15:51 +0300

    Fixed

--- public/pages/home/industriesSwiper/industriesSwiper.js
+++ public/pages/home/industriesSwiper/industriesSwiper.js
@@ -1,18 +1,20 @@
 const swiperComponent = {
-    swiper: document.getElementById('swiperIndustries'),
-    buttonPrev: document.getElementById('industrySliderButtonPrev'),
-    buttonNext: document.getElementById('industrySliderButtonNext'),
-}
+  swiper: document.getElementById('swiperIndustries'),
+  buttonPrev: document.getElementById('industrySliderButtonPrev'),
+  buttonNext: document.getElementById('industrySliderButtonNext'),
+};
 
-
-const swiperIndustriesInit = new Swiper(swiperComponent.swiper, {
+function swiperIndustriesInit() {
+  const swiperIndustries = new Swiper(swiperComponent.swiper, {
     speed: 400,
     spaceBetween: 0,
     slidesPerView: 1,
     loop: true,
     navigation: {
-        nextEl: swiperComponent.buttonNext,
-        prevEl: swiperComponent.buttonPrev,
+      nextEl: swiperComponent.buttonNext,
+      prevEl: swiperComponent.buttonPrev,
     },
   });
+}
 
+export { swiperIndustriesInit };
