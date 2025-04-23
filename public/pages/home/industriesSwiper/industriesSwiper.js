96848b023690bb0319255ff0946d1a225eb3c6d6
Kiltsov Anton <kiltsov.anton@gmail.com>
2025-04-23 14:51:39 +0300

    Fixed

--- public/pages/home/industriesSwiper/industriesSwiper.js
+++ public/pages/home/industriesSwiper/industriesSwiper.js
@@ -1,29 +1,31 @@
 const industrySliderConfig = {
-  '#sliderIndustries1': { slidesPerView: 1 },
-  '#sliderIndustries2': { slidesPerView: 2 },
-  '#sliderIndustries3': { slidesPerView: 3 },
-  '#sliderIndustries4': { slidesPerView: 4 },
-  '#sliderIndustries5': { slidesPerView: 5 },
+    'sliderIndustries1': { slidesPerView: 1 },
+    'sliderIndustries2': { slidesPerView: 2 },
+  'sliderIndustries3': { slidesPerView: 3 },
+  'sliderIndustries4': { slidesPerView: 4 },
+  'sliderIndustries5': { slidesPerView: 5 },
 };
 
-function initIndustrySlider(sliderRoot, { slidesPerView = 1 }) {
-  const swiperEl = sliderRoot.querySelector('.swiper');
-  const buttonNext = sliderRoot.querySelector('[data-next]');
-  const buttonPrev = sliderRoot.querySelector('[data-prev]');
-  const buttonContainer = sliderRoot.querySelector('.industry-slider__buttons');
-  const buttonTrack = sliderRoot.querySelector('.industry-slider__button-track');
-  const buttons = sliderRoot.querySelectorAll('.industry-slider__button');
-
-  new Swiper(swiperEl, {
-    speed: 700,
-    spaceBetween: 0,
-    slidesPerView,
-    loop: true,
-    navigation: {
-      nextEl: buttonNext,
-      prevEl: buttonPrev,
-    },
-  });
+function initIndustrySlider(sliderRoot, config = {}) {
+    const swiperEl = sliderRoot.querySelector('.swiper');
+    const buttonNext = sliderRoot.querySelector('[data-next]');
+    const buttonPrev = sliderRoot.querySelector('[data-prev]');
+    const buttonContainer = sliderRoot.querySelector('.industry-slider__buttons');
+    const buttonTrack = sliderRoot.querySelector('.industry-slider__button-track');
+    const buttons = sliderRoot.querySelectorAll('.industry-slider__button');
+  
+    const slidesPerView = config.slidesPerView || 1;
+  
+    new Swiper(swiperEl, {
+      speed: 700,
+      spaceBetween: 0,
+      slidesPerView: slidesPerView,
+      loop: true,
+      navigation: {
+        nextEl: buttonNext,
+        prevEl: buttonPrev,
+      },
+    });
 
   // Move buttons
   buttonContainer.addEventListener('mousemove', (e) => {
@@ -59,12 +61,12 @@ function initIndustrySlider(sliderRoot, { slidesPerView = 1 }) {
 }
 
 function swiperIndustriesInit() {
-  const sliders = document.querySelectorAll('.industry-slider');
-  sliders.forEach((slider) => {
-    const id = `#${slider.id}`;
-    const config = industrySliderConfig[id] || { slidesPerView: 1 };
-    initIndustrySlider(slider, config);
-  });
-}
+    const sliders = document.querySelectorAll('.industry-slider');
+    sliders.forEach((slider) => {
+      const sliderId = slider.id;
+      const config = industrySliderConfig[sliderId] || { slidesPerView: 1 };
+      initIndustrySlider(slider, config);
+    });
+  }
 
 export { swiperIndustriesInit };
