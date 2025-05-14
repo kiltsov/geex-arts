4120f7bd4fb1217e198def11397665ca098bf13a
Kiltsov Anton <kiltsov.anton@gmail.com>
2025-05-14 11:38:07 +0300

    Fixed

--- public/components/jetadmin/jetadmin.js
+++ public/components/jetadmin/jetadmin.js
@@ -1,1 +1,52 @@
-console.log('jet admin script');
+function swiperBuildInit() {
+  const swiperBuild = new Swiper('#swiperBuild', {
+    spaceBetween: 8,
+    slidesPerView: 5,
+    slidesPerGroup: 5,
+
+    observer: true,
+    observeParents: true,
+
+    grabCursor: true,
+    a11y: false,
+    allowTouchMove: true,
+
+    loop: true,
+	speed: 600,
+
+    navigation: {
+      nextEl: '[build-form=next]',
+    //   prevEl: '[h-blog-button=prev]',
+    },
+
+    // pagination: {
+    //   el: '.new-temp-pagination',
+    //   clickable: true,
+    // },
+
+    // breakpoints: {
+    //   992: {
+    //     spaceBetween: 0,
+    //     slidesPerView: 0,
+    //     slidesPerGroup: 3,
+    //   },
+    //   768: {
+    //     spaceBetween: 0,
+    //     slidesPerView: 1,
+    //     slidesPerGroup: 1,
+    //   },
+    //   480: {
+    //     spaceBetween: 0,
+    //     slidesPerView: 1,
+    //     slidesPerGroup: 1,
+    //   },
+    //   0: {
+    //     spaceBetween: 0,
+    //     slidesPerView: 1,
+    //     slidesPerGroup: 1,
+    //   },
+    // },
+  });
+}
+
+swiperBuildInit();
