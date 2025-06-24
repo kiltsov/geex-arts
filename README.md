2754973e6b2b4e7059bf5df6380ce9b0d3989746
Kiltsov Anton <kiltsov.anton@gmail.com>
2025-06-24 17:09:09 +0300

    Remove all .DS_Store files from repository

--- README.md
+++
@@ -1,1 +0,0 @@
-### Geex Arts
--- public/global/global.js
+++
@@ -1,4 +0,0 @@
-// IS MOBILE CONDITION
-export function isMobile() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-}
